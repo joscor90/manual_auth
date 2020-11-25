dca67bee0a179d0ce5e153072755026b94f5723c
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-25 11:40:50 -0300

    Sign in, sign up and sign out now working. Add foreign key to stories that references user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
     def new 
+        @user = User.new
     end 
     
     def create 
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: 'User was successfully created'
+        else
+            render :new, notice: "Something went wrong"
+        end
     end 
 
     def show 
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    protected
+    def current_user 
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged? 
+        session[:user_id].present?
+    end
 end
